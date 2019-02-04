2c1e6f1cd9664279ccbe236adb6f64a50ee3f557
vlvanchin <vlvanchin@gmail.com>
2019-02-04 09:37:31 +0800

    site name updated to 'nature-inspired'
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>

--- deploy_hugo.sh
+++ deploy_hugo.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/zsh -e
 #
-# to deploy public contents to vlvanchin.github.io/nature-bites
+# to deploy public contents to vlvanchin.github.io/nature-inspired
 #
-echo -e "\nDeploying website contents to vlvanchin.github.io/nature-bites"
+echo -e "\nDeploying website contents to vlvanchin.github.io/nature-inspired"
 
-echo -e "\tdeleting public folder in nature-bites-src"
+echo -e "\tdeleting public folder in nature-inspired-src"
 if [ -d "public" ]; then 
   rm -fr public
 fi
 
-#echo -e "\tdeleting old contents in vlvanchin.github.io/nature-bites"
-cd ../nature-bites
+#echo -e "\tdeleting old contents in vlvanchin.github.io/nature-inspired"
+cd ../nature-inspired
 if [ $(ls -al | wc -l) -gt 4 ] ; then
   rm -fr * 
 fi
 
-cd ../nature-bites-src
+cd ../nature-inspired-src
 
 echo -e "\tbuilding site with hugo"
-hugo -d ../nature-bites
+hugo -d ../nature-inspired
 
 echo -e "\tcommiting to nature-bites repo"
-cd ../nature-bites
+cd ../nature-inspired
 git add .
 git commit -s -m "website updated"
 git push origin master

d53c6e1bea18afbf939e915c5081d71c780e084c
vlvanchin <vlvanchin@gmail.com>
2019-02-05 10:41:01 +0800

    removed public/ folder and updatd deploy_hugo.sh to push only to AWS
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>

--- deploy_hugo.sh
+++ deploy_hugo.sh
@@ -1,40 +1,25 @@
 #!/usr/bin/zsh -e
 #
-# to deploy public contents to vlvanchin.github.io/nature-inspired
+# to deploy public contents to AWS
 #
-echo -e "\nDeploying website contents to vlvanchin.github.io/nature-inspired"
+echo -e "\nDeploying website contents to AWS"
 
 echo -e "\tdeleting public folder in nature-inspired-src"
 if [ -d "public" ]; then 
   rm -fr public
 fi
 
-#echo -e "\tdeleting old contents in vlvanchin.github.io/nature-inspired"
-cd ../nature-inspired
-
-git checkout master
-if [ $(ls -al | wc -l) -gt 4 ] ; then
-  rm -fr * 
-fi
-
-cd ../nature-inspired-src
-
-git checkout master
 echo -e "\tbuilding site with hugo"
-hugo -d ../nature-inspired
+hugo 
 
-echo -e "\tcommiting to nature-bites repo"
-cd ../nature-inspired
+echo -e "\tcommiting to nature-inspired-src repo"
 git add .
 git commit -s -m "website updated"
 git push origin master
-cd ../nature-inspired-src
 
-cd ..
 echo -e "\tuploading to AWS s3 bucket 'nature-inspired'"
 echo -e "\t\tuse --dryrun option to test"
-aws s3 cp --recursive nature-inspired s3://nature-inspired/ 
-cd nature-inspired-src
+aws s3 cp --recursive public s3://nature-inspired/ 
 
 echo "script done"
 
